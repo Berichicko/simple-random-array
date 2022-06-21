053a25923a6eebc906604046311620117a7c96fc
Aliaksandr Zasinets <aliaksandr.zasinets@gmail.com>
2022-06-21 07:39:37 +0300

    checking code for exceptions

--- src/main/java/by/zasinets/qa/Array.java
+++ src/main/java/by/zasinets/qa/Array.java
@@ -8,6 +8,10 @@ public class Array implements Multiplicity {
     public int[] createArray() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите размер массива: ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Введено некорректное значение! Попробуйте еще раз!");
+            scanner.nextLine();
+        }
         int size = scanner.nextInt();
         int[] myArray = new int[size];
         return myArray;
